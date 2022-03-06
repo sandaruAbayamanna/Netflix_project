df2fbac5a2ce0dcd505d9763f038d8697ddb2d09
sandaru <sandaruabayamanna@gmail.com>
2022-03-06 13:47:16 +0530

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="container"></div>
+        <div className="left"></div>
+        
+    </div>
+  )
+}
+
+export default Navbar
