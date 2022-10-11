754047b194062079373460380eff6953c71c41fd
sandaru <sandaruabayamanna@gmail.com>
2022-10-11 19:21:00 +0530

    done login & register api

--- api/index.js
+++ api/index.js
@@ -1,15 +1,26 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose =require("mongoose")
-const dotenv = require("dotenv")
+const mongoose =require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
+    
+
 }).then(()=> console.log("DB Connection Successful!"))
-.catch((err) =>console.log(err))
+.catch((err) =>{
+    console.error(err);
+});
+
+//to accept json inputs
+app.use(express.json());
+
+//endpoint for authentication route
+app.use("/api/auth", authRoute);
 
 app.listen(8800,() =>{
     console.log("Backend Server is running")
